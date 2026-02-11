28d74818fa6e72c46cf57279e9e24ea0d473b645
tomatojuice <tomato.juice.7@gmail.com>
2026-02-11 22:45:40 +0900

    update

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,22 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
- * @type {import('@react-native/metro-config').MetroConfig}
+ * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // 1. スマホで使えない機能を、使えるライブラリに置き換える（ポリフィル）
+    extraNodeModules: {
+      crypto: require.resolve('react-native-crypto'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+      events: require.resolve('events'),
+      process: require.resolve('process/browser'),
+    },
+  },
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
